1bde5eeacc5280ff3201ff7fa95c834267e7fbc5
Malloumdavid0811 <davidguendergue@gmail.com>
2026-02-22 20:52:37 +0100

    Initial commit

---
+++ src/app/laptop-list.ts
@@ -0,0 +1,71 @@
+import { LaptopList } from './laptop.model';
+
+export const LAPTOP_LIST: LaptopList = [
+
+  {
+    id: 1,
+    name: 'Call of Duty Modern Warfare III',
+    picture:
+      'https://m.media-amazon.com/images/I/814L+lx15uL._AC_SL1500_.jpg',
+    number: 1,
+
+  },
+  {
+    id: 2,
+    name: 'Gran Turismo 7 | Standard Edition PS4',
+    picture:
+      'https://m.media-amazon.com/images/I/818gNzcTz9L._AC_SL1500_.jpg',
+    number: 1,
+  },
+  {
+    id: 3,
+    name: "Marvel's Avengers PS4",
+    picture:
+      'https://m.media-amazon.com/images/I/713+Epx3IwL._AC_SL1500_.jpg',
+    number: 1,
+  },
+  {
+    id: 4,
+    name: 'Dark Souls Trilogy - PS4',
+    picture:
+      'https://m.media-amazon.com/images/I/71G4xW5VmxL._AC_SL1308_.jpg',
+    number: 1,
+    
+  },
+  {
+    id: 5,
+    name: 'Bandai Namco Entertainment Elden Ring - Standard Edition PS4',
+    picture:
+      'https://m.media-amazon.com/images/I/816WfW+a5DL._AC_SL1500_.jpg',
+    number: 1,
+  },
+  {
+    id: 6,
+    name: 'EA SPORTS FC 25 Standard Edition XBOX Series X|S / XBOX One | Jeu Vidéo | Français',
+    picture:
+      'https://m.media-amazon.com/images/I/81sRNFc4RmL._AC_SL1500_.jpg',
+    number: 1,
+  },
+  {
+    id: 7,
+    name: 'EA SPORTS FC 25 Standard Edition PS4 | Jeu Vidéo | Français',
+    picture:
+      'https://m.media-amazon.com/images/I/81PUqiYgXFL._AC_SL1500_.jpg',
+    number: 1,
+  },
+  {
+    id: 8,
+    name: 'Ghost Recon: Breakpoint - Edition Gold PS4',
+    picture:
+      'https://m.media-amazon.com/images/I/71C-b9G5kYL._AC_SL1000_.jpg',
+    number: 1,
+  },
+  {
+    id: 9,
+    name: 'Cobra Kai 2 Dojos rising PS4 ',
+    picture:
+      'https://m.media-amazon.com/images/I/81keQyzGm6L._AC_SL1500_.jpg',
+    number: 1,
+  },
+
+];
---
+++ src/app/laptop.model.ts
@@ -0,0 +1,8 @@
+export interface Laptop {
+    id: number;
+    name: string;
+    picture: string;
+    number: number;
+}
+
+export type LaptopList = Laptop[]
